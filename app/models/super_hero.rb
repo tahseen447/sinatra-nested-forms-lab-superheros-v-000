dd94a95bb907af03b1b3e6633a3379709ff5935b
tahseen447 Siddiqui <tahseenfatima1986@gmail.com>
2018-09-09 19:04:16 +0000

    automatically backed up by learn

--- app/models/super_hero.rb
+++ app/models/super_hero.rb
@@ -1,4 +1,17 @@
-class SuperHero 
+class SuperHero
 
   attr_accessor :name, :bio, :power
   @@all = []
+
+  def initialize(params)
+    @name = params[:members][:name]
+    @bio = params[:members][:bio]
+    @power = params[:members][:power]
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+end
