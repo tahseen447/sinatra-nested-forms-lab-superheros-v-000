edaee06eb381eb2723feebcfb044d472164a2a1a
tahseen447 Siddiqui <tahseenfatima1986@gmail.com>
2018-09-09 19:28:21 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,6 +12,7 @@ class App < Sinatra::Base
 
     post '/teams' do
       @team = Team.new(params[:team])
+      params[:team][:members].inspect
       params[:team][:members].each do |member|
         puts "here"
         SuperHero.new(member)
